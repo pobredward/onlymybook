c0629c07d01463f9dc2c4117a0acea7536dc355d
pobredward <pobredward@gmail.com>
2025-05-21 22:29:44 +0900

    Add Metatag

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -26,6 +26,7 @@ export const MainLayout: React.FC<MainLayoutProps> = ({
         <meta property="og:site_name" content="디지털 자서전" />
         <link rel="icon" href="/favicon.ico" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5100840159526765" crossOrigin="anonymous"></script>
+        <meta name="google-adsense-account" content="ca-pub-5100840159526765" />
       </Head>
       
       <div className="flex flex-col min-h-screen">
